c55e17d2c24267220b8c7f3ef21276df0a366698
Pedro Martins <mourao.martins@gmail.com>
2016-11-28 14:36:12 +0000

    Add project files.

---
+++ WebApplicationVersioningDemos/Controllers/PresentationController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApplicationVersioningDemos.Controllers
+{
+    public class PresentationController : Controller
+    {
+        // GET: Presentation
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Presentation/Why
+        public ActionResult Why()
+        {
+            return View();
+        }
+
+        // GET: Presentation/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Presentation/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Presentation/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Presentation/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Presentation/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Presentation/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ WebApplicationVersioningDemos/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebApplicationVersioningDemos.Startup))]
+namespace WebApplicationVersioningDemos
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
